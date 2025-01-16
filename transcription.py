b1f7ca9a821edb663386ab0b719ce015af7ec9d9
Eryk Kunasz <eryk.kunasz@gmail.com>
2025-01-16 20:55:56 +0100

    Modyfikacja prompta

--- transcription.py
+++ transcription.py
@@ -80,12 +80,31 @@ class TranscriptionManager:
         return self.temp_audio_file
 
     def send_to_ollama(self, text):
+        prompt = f"""
+                    Your output should use the following template:
+                    ### Summary
+                    ### Analogy
+                    ### Notes
+                    - [Emoji] Bulletpoint
+                    ### Keywords
+                    - Explanation
+                    You have been tasked with creating a concise summary of a YouTube video using its transcription.
+                    Make a summary of the transcript.
+                    Additionally make a short complex analogy to give context and/or analogy from day-to-day life from the transcript.
+                    Create 10 bullet points (each with an appropriate emoji) that summarize the key points or important moments from the video's transcription.
+                    In addition to the bullet points, extract the most important keywords and any complex words not known to the average reader as
+                    well as any acronyms mentioned. For each keyword and complex word, provide an explanation and definition based on its occurrence in the transcription.
+                    Please ensure that the summary, bullet points, and explanations fit within the 330-word limit, while still offering a comprehensive and clear understanding of the video's content.
+                    Use the text above: {text}.
+                    """
+                    
         try:
             response = requests.post(
                 "http://localhost:11434/api/generate",
                 json={
                     "model": "mistral:latest",
-                    "prompt": f"Stresć mi ten tekst: {text}"
+                    "prompt": prompt,
+                    # "prompt": f"Stresć mi ten tekst: {text}"
                 },
                 stream=True
             )
